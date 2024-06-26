89d92301f667a65dccf182ef72585b31c5ab393f
wi <whyeil29@gmail.com>
2024-06-26 18:34:38 +0900

    0626

---
+++ 09_page/a-3/script/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    // 탭
+    $("#tabs>ul li").click(function () {
+        var i = $(this).index();
+        $("#tabs>div").css({
+            display: "none"
+        });
+        $("#tabs>div").eq(i).css({
+            display: "block"
+        });
+    });
+   
+    // 모달 팝업
+    $("#tabs>div li").eq(0).click(function(){
+        $("#modal").show();
+    });
+    $(".popup button").click(function(){
+        $("#modal").hide();
+    });
+});
+
+
+
+
+
+
+
+
+
