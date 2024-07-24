52965359b90f66dd665c564a410dce2594fc46ad
wi <whyeil29@gmail.com>
2024-07-25 00:45:31 +0900

    0725

--- 70_pf/daewoo/js/multi_lang.js
+++ 70_pf/daewoo/js/multi_lang.js
@@ -23,11 +23,11 @@ const ml = {
 
 window.addEventListener("DOMContentLoaded", ()=>{
     const sel = document.querySelector("select");
-    const logo = document.querySelector(".logo");
+    const logo = document.querySelector(".logo img");
     const gnb = document.querySelectorAll(".gnb a");
-    const tit = document.querySelectorAll(".tit");
-    const flogo = document.querySelector(".flogo");
-    const fmenu = document.querySelectorAll(".fmenu");
+    const tit = document.querySelectorAll(".tit span");
+    const flogo = document.querySelector(".flogo img");
+    const fmenu = document.querySelectorAll(".fmenu a");
     const addr = document.querySelector(".addr");
 
     //console.log(sel,logo,gnb,tit,flogo,fmenu,addr);
@@ -37,11 +37,26 @@ window.addEventListener("DOMContentLoaded", ()=>{
         let data = ml[opt];
 
         //로고 변경
-        logo.setAttribute("src",`${data["logo"]}"`);
+        logo.setAttribute("src",`${data["logo"]}`);
         console.log(data["logo"]);
+
+        // 메인 타이틀 변경
+        tit.forEach((ele,idx)=>
+            ele.innerText = data["tit"][idx]);
 
         // 메뉴 변경
         gnb.forEach((ele,idx)=>
             ele.innerText = data["gnb"][idx]);
+
+        // 푸터 메뉴 변경
+        fmenu.forEach((ele,idx)=>
+            ele.innerText = data["fmenu"][idx]);
+
+        //푸터 로고 변경
+        flogo.setAttribute("src",`${data["flogo"]}`);
+        console.log(data["flogo"]);
+
+        // 주소 변경
+        addr.innerText = data["addr"];
     };
 });
