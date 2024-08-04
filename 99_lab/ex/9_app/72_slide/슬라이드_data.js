2713ee3e91f1120805c6423e74a1c79535649d1e
wi <whyeil29@gmail.com>
2024-08-04 21:59:23 +0900

    0804

---
+++ 99_lab/ex/9_app/72_slide/슬라이드_data.js
@@ -0,0 +1,23 @@
+// 데이터 정의
+const imgObj = {
+    imgData1: {
+        imgSrc: '/assets/images/pic_1.jpg',
+        imgAlt: '하늘'
+    },
+    imgData2: {
+        imgSrc: '/assets/images/pic_2.jpg',
+        imgAlt: '산'
+    },
+    imgData3: {
+        imgSrc: '/assets/images/pic_3.jpg',
+        imgAlt: '로보트'
+    },
+    imgData4: {
+        imgSrc: '/assets/images/pic_4.jpg',
+        imgAlt: '자동차'
+    },
+    imgData5: {
+        imgSrc: '/assets/images/pic_5.jpg',
+        imgAlt: '바다'
+    },
+}
