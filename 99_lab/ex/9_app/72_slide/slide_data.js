1526b3c6ac0fe04de77e083d2d94e33c0977264d
wi <whyeil29@gmail.com>
2024-08-07 02:03:11 +0900

    0807

--- 99_lab/ex/9_app/72_slide/slide_data.js
+++ 99_lab/ex/9_app/72_slide/slide_data.js
@@ -1,23 +1,23 @@
 // 데이터 정의
 const imgObj = {
     imgData1: {
-        imgSrc: '/assets/images/pic_1.jpg',
+        imgSrc: '/assets/pic_1.jpg',
         imgAlt: '하늘'
     },
     imgData2: {
-        imgSrc: '/assets/images/pic_2.jpg',
+        imgSrc: '/assets/pic_2.jpg',
         imgAlt: '산'
     },
     imgData3: {
-        imgSrc: '/assets/images/pic_3.jpg',
+        imgSrc: '/assets/pic_3.jpg',
         imgAlt: '로보트'
     },
     imgData4: {
-        imgSrc: '/assets/images/pic_4.jpg',
+        imgSrc: '/assets/pic_4.jpg',
         imgAlt: '자동차'
     },
     imgData5: {
-        imgSrc: '/assets/images/pic_5.jpg',
+        imgSrc: '/assets/pic_5.jpg',
         imgAlt: '바다'
     },
 }
