ffd22f38541089b7046f0ca7b280b4e1d14711a1
wi <whyeil29@gmail.com>
2024-08-18 01:45:50 +0900

    auto commit

--- 97_react/03_jsx.jsx
+++ 97_react/03_jsx.jsx
@@ -1,19 +1,20 @@
-// 변수에 태그를 할당한다.
-// 최상위 부모는 1개 이어야 한다.
+// 변수 선언
+const ele1 = <h2>JSX 사용</h2>;
 
-const myelement = (
-    <table>
-        <tr>
-            <th>Name</th>
-        </tr>
-        <tr>
-            <td>John</td>
-        </tr>
-        <tr>
-            <td>Elsa</td>
-        </tr>
-    </table>
-);
+/* 
+    ReactDOM
+    createRoot() ~ React 구성 요소가 표시될 HTML 요소 정의 메소드 
+    render()
 
-// 출력
-ReactDOM.render(myelement, document.querySelector("#root"));
+    [출력 방법1]
+        ReactDOM.render(요소, 컨테이너);
+    [출력 방법2]
+        const 변수 = ReactDOM.createRoot(컨테이너);
+        변수.render(요소);
+*/
+const root1 = ReactDOM.createRoot(document.querySelectorAll("#root div")[0]);
+root1.render(ele1);
+
+const ele2 = React.createElement("h2",{},"JSX 사용 안함");
+const root2 = ReactDOM.createRoot(document.querySelectorAll("#root div")[1]);
+root2.render(ele2);
