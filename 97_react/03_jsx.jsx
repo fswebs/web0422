a39b5ba0948b38c969f003aa9e2759e9065bf33e
wi <whyeil29@gmail.com>
2024-08-18 02:45:54 +0900

    auto commit

--- 97_react/03_jsx.jsx
+++ 97_react/03_jsx.jsx
@@ -1,5 +1,5 @@
 // 변수 선언
-const ele1 = <h2>JSX 사용</h2>;
+const myElement1 = <h2>I do use JSX!</h2>;
 
 /* 
     ReactDOM
@@ -13,8 +13,36 @@ const ele1 = <h2>JSX 사용</h2>;
         변수.render(요소);
 */
 const root1 = ReactDOM.createRoot(document.querySelectorAll("#root div")[0]);
-root1.render(ele1);
+root1.render(myElement1);
 
-const ele2 = React.createElement("h2",{},"JSX 사용 안함");
+// createElement("태그", {표현식}, 내용) ~ React 요소 생성 메소드
+const myElement2 = React.createElement("h2", {}, "I do not use JSX!");
 const root2 = ReactDOM.createRoot(document.querySelectorAll("#root div")[1]);
-root2.render(ele2);
+root2.render(myElement2);
+
+// JSX 표현식 - { } 안에 React 변수, 속성, JS 표현식을 사용한다.
+const num = 5;
+const myElement3 = <h2>React is {num + num} times better with JSX</h2>;
+const root3 = ReactDOM.createRoot(document.querySelectorAll("#root div")[2]);
+root3.render(myElement3);
+
+// 최상위 요소는 하나
+// 최상위 요소는 HTML 태그, <React.Fragment></React.Fragment> 또는 <></>로 한다.
+const myElement4 = (
+    <React.Fragment>
+        <p>I am a paragraph.</p>
+        <p>I am a paragraph too.</p>
+    </React.Fragment>
+);
+const root4 = ReactDOM.createRoot(document.querySelectorAll("#root div")[3]);
+root4.render(myElement4);
+
+// 빈 요소는 />로 닫는다.
+const myElement5 = <input type="text" />;
+const root5 = ReactDOM.createRoot(document.querySelectorAll("#root div")[4]);
+root5.render(myElement5);
+
+// class 대신 className으로 기록한다.
+const myElement6 = <h2 className="myclass">Hello World</h2>;
+const root6 = ReactDOM.createRoot(document.querySelectorAll("#root div")[5]);
+root6.render(myElement6);
