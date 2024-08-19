f02121efd7fc1e31a963fb4216c6199c07bbbac3
wi <whyeil29@gmail.com>
2024-08-19 21:27:58 +0900

    0819

--- 97_react/06_event.jsx
+++ 97_react/06_event.jsx
@@ -6,6 +6,7 @@
     <태그 onClick={함수}>
 */
 
+// 이벤트 추가
 function Football() {
     const shoot = () => {
         alert("Great Shot!");
@@ -16,5 +17,17 @@ function Football() {
     );
 }
 
+// 인수 전달
+// 이벤트 핸들러에 인수를 전달하려면 화살표 함수를 사용한다.
+function Football2() {
+    const shoot = (a) => {
+      alert(a);
+    }
+  
+    return (
+      <button onClick={() => shoot("Goal!")}>Take the shot!</button>
+    );
+  }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+root.render(<><Football /><Football2 /></>);
