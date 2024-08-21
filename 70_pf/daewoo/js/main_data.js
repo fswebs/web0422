342a95ab08ac51a77dc398dbca90900fe78fd7a1
wi <whyeil29@gmail.com>
2024-08-21 14:08:48 +0900

    0821

---
+++ 70_pf/daewoo/js/main_data.js
@@ -0,0 +1,24 @@
+// 다국어 객체
+const ml = {
+    // 영어
+    "en": {
+        "logo": "assets/en/logo.png",
+        "gnb": ["About Us", "Business", "PR Center", "Contact"],
+        "tit": ["GREAT", "POSSIBILITY"],
+        "flogo": "assets/en/footer_logo.png",
+        "fmenu": ["About DAEWOO E&C", "Sitemap","Contact", "Email Security"],
+        "addr": "04548 DAEWOO E&C 170 Eulji-ro, Jung-gu, Seoul, Republic of Korea"
+
+    },
+    // 한국어
+    "ko": {
+        "logo": "assets/ko/logo.png",
+        "gnb": ["회사소개", "사업분야", "홍보센터", "고객센터"],
+        "tit": ["BUILD", "TOGETHER"],
+        "flogo": "assets/ko/footer_logo.png",
+        "fmenu": ["회사소개", "사이트맵","개인정보처리방침", "이메일수집거부"],
+        "addr": "04548 서울특별시 중구 을지로 170 ㈜대우건설"
+    }
+}
+
+export default ml;
