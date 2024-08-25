3972605c0174218f51f4c58c22b70c3240ea3d22
wi <whyeil29@gmail.com>
2024-08-26 02:43:06 +0900

    0826

--- 97_react/cra/my-app2/src/routes/Home.js
+++ 97_react/cra/my-app2/src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
     const [loading, setLoading] = useState(true);
@@ -32,17 +33,18 @@ function Home() {
 
     console.log(movies);
     return (
-        <div>
+        <div className={styles.container}>
             {loading ? (
                 <h1>로딩 중...</h1>
             ) : (
-                <div>
+                <div className={styles.movies}>
                     {movies.map((movie) => (
                         // Movie({id:movie})
                         <Movie
                             //key는 ReactJS에서만 map안에서 component들을 render할 때 사용한다.
                             key={movie.id}
                             id={movie.id}
+                            year={movie.year}
                             coverImg={movie.medium_cover_image}
                             title={movie.title}
                             summary ={movie.summary}
