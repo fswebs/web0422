9452449ea47929441742d2a6f3d3a9abf17ac1ca
wi <whyeil29@gmail.com>
2024-08-26 10:55:09 +0900

    0826

--- 97_react/pro/src/App.jsx
+++ 97_react/pro/src/App.jsx
@@ -1,9 +1,23 @@
 // src/App.js
 
+// 1. 컴포넌트(일종의 함수) 
+// 예) App, Card
+// - 첫 글자는 대문자로
+// - 반드시 return 키워드로 시작한다.
+// - JSX 언어를 사용한다.
+// - JSX? JS + XML(HTML+사용자가 태그를 만들어 쓴다.)
 function App() {
     return (
         <div>
+            {/* XML 영역에서 생긴 주석 */}
             {cards.map((card, index) => (
+                // {표현식}
+                // 표현식 -> 변수, 값, JS문장(조건 연산자)
+                // 제어문은 사용할 수 없다.
+                // <App />
+                // <Card 속성="문자열" 속성={변수} /> -> Card 컴포넌트
+                // function Card() {}
+                // XML
                 <Card
                     key={index}
                     title={card.title}
--- 97_react/pro/src/Card.jsx
+++ 97_react/pro/src/Card.jsx
@@ -1,7 +1,21 @@
 // src/Card.js
 
-export function Card({ title, text, image }) {
+// Card 컴포넌트
+// 컴포넌트 첫글자는 대문자로 기록한다.
+// return으로 시작한다.
+// function 컴포넌트명(속성) {
+//      return (
+//           XML코드
+//           class -> className
+//      );    
+// }
+// props -> properies
+// 매개변수를 객체 형식 {값, 값, 값, ...}
+
+
+function Card({ title, text, image }) {
     return (
+        // {표현식} -> 표현식은 변수, 연산자 등 JS문장 (제어문x) -> if제어문 대신 삼항연산자를 사용한다.
         <div className="card">
             <img src={image} alt={title}/>
             <div className="card-body">
@@ -14,3 +28,8 @@ export function Card({ title, text, image }) {
         </div>
     );
 }
+
+// 파일 단위 모듈화
+// import: 파일 가져오기
+// export: 파일 내보내기
+export default Card;
