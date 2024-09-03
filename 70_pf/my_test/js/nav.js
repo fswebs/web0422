b9d9d839f26c92e4b9d1869aa3e6770892018582
wi <whyeil29@gmail.com>
2024-09-04 02:04:18 +0900

    0904

--- 70_pf/my_test/js/nav.js
+++ 70_pf/my_test/js/nav.js
@@ -1,13 +1,33 @@
 // nav.js
 $(()=>{
     const nav = $("nav");
+    const nav2 = $("nav").clone().appendTo("body").css({top:0});
+    nav2.css({opacity:1});
+    nav.removeClass("on");
 
     $(window).on("scroll", function () {
-        console.log(this.scrollTop());
-        if (this.scrollTop() >= 50) {
-            nav.addClass("active");
+        if ($(this).scrollTop() >= 200) {
+            nav2.css({opacity:0});
+            nav.addClass("on");
         } else {
-            nav.removeClass("active");
+            nav2.css({opacity:1});
+            nav.removeClass("on");
         }
+    });
+
+
+    const collapsedMenuIcon = $(".collapsedMenuIcon");
+    const navbarCollapse = $(".navbar-collapse");
+    const navbarBrand = $(".navbar-brand");
+    const navLink = $(".nav-link");
+
+    collapsedMenuIcon.click(()=>{
+        $(event.currentTarget).toggleClass("change");
+    });
+    navLink.click(()=>{
+        navbarCollapse.removeClass("show");
+    });
+    navbarBrand.click(()=>{
+        navbarCollapse.removeClass("show");
     });
 });
