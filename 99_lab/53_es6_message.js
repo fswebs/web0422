9f548454642ac8c6b20d34419a04f4f3510f4499
wi <whyeil29@gmail.com>
2024-09-05 22:02:12 +0900

    0906

--- 99_lab/53_es6_message.js
+++
@@ -1,8 +0,0 @@
-const message = () => {
-    const name = "Jesse";
-    const age = 40;
-    return name + " is " + age + " years old.";
-};
-
-// 기본 내보내기
-export default message;
--- 99_lab/53_es6_person.js
+++
@@ -1,8 +0,0 @@
-// export const name = "Jessica";
-// export const age = 50;
-
-const name = "Jesse";
-const age = 40;
-
-// 명명된 내보내기
-export {name, age};
